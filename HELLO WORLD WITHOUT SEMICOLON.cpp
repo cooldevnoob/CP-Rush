5d4e67fb08d66d5e8c54c804045f94ac0ffef926
cooldevnoob <56195799+cooldevnoob@users.noreply.github.com>
2019-10-05 18:56:07 +0530

    Add files via upload

---
+++ HELLO WORLD WITHOUT SEMICOLON.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define tc(t) int t;cin >> t; while(t--)
+#define pb push_back
+#define fi first
+#define se second
+#define debug1(x) cout<<#x<<" = "<<x<<'\n';
+#define debug2(x, y) cout << #x << " = " << x << "   " << #y << " = " << y << "\n";
+#define _ ios_base::sync_with_stdio(0);cin.tie(NULL);
+
+int main() {_
+	if(printf("IT WORKS")) {
+		
+	}
+	return 0;
+}
+
